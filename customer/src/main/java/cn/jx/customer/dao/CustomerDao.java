c48cb700b514a8c728d9198c6f8d76c526a5da80
colin <colinjx.163.com>
2018-08-19 21:13:38 +0800

    分页完成

---
+++ customer/src/main/java/cn/jx/customer/dao/CustomerDao.java
@@ -0,0 +1,10 @@
+package cn.jx.customer.dao;
+
+import cn.jx.common.dao.BaseDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends BaseDao {
+
+
+}
